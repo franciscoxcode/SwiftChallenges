856ae1ee12b52025d3943d4d5a2db68e78159e71
Francisco Casillas <chiccasillas@gmail.com>
2025-05-09 15:35:06 -0600

    Solve Remove First And Last from String Kata

--- SwiftChallenges.playground/Pages/8 Fundamentals.xcplaygroundpage/Contents.swift
+++ SwiftChallenges.playground/Pages/8 Fundamentals.xcplaygroundpage/Contents.swift
@@ -10,8 +10,16 @@ import Foundation
 //14: -14
 //-34: 34
 
-func opposite(_ number: Double) -> Double {
-    -number
+//func opposite(_ number: Double) -> Double {
+//    -number
+//}
+//
+//print(opposite(6))
+
+//It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+
+func removeFirstAndLast(_ str: String) -> String {
+  String(str.dropFirst().dropLast())
 }
 
-print(opposite(6))
+print(removeFirstAndLast("Loquillo"))
