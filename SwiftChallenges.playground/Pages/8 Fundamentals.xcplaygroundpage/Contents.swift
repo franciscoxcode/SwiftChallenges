d9f6647584c1d3407fd8fc8de1474010753b8471
Francisco Casillas <chiccasillas@gmail.com>
2025-05-13 12:17:05 -0600

    Calculate miliseconds from midnight

--- SwiftChallenges.playground/Pages/8 Fundamentals.xcplaygroundpage/Contents.swift
+++ SwiftChallenges.playground/Pages/8 Fundamentals.xcplaygroundpage/Contents.swift
@@ -52,3 +52,26 @@ import Foundation
 //}
 //
 //print(playerMoves(3, 6))
+//
+//Clock shows h hours, m minutes and s seconds after midnight.
+//
+//Your task is to write a function which returns the time since midnight in milliseconds.
+//
+//Example:
+//
+//h = 0
+//m = 1
+//s = 1
+//
+//result = 61000
+//Input constraints:
+//
+//0 <= h <= 23
+//0 <= m <= 59
+//0 <= s <= 59
+
+func timeSinceMidnight(_ h:Int, _ m:Int, _ s: Int) -> Int {
+    (h*60*60*1000)+(m*60*1000)+(s*1000)
+}
+
+print(timeSinceMidnight(0,1,1))
